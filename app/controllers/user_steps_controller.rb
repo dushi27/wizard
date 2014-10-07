aaea040a032a05974addb4721a342174a33da388
dushi27 <dushi27@gmail.com>
2014-10-07 21:08:30 +0000

    set up 3 step wizard

--- app/controllers/user_steps_controller.rb
+++ app/controllers/user_steps_controller.rb
@@ -3,6 +3,22 @@ class UserStepsController < ApplicationController
   steps :personal, :social
   
   def show
+    @user = User.find(session[:user_id])
     render_wizard
+  end
+  
+  def update
+    @user = User.find(session[:user_id])
+    @user.update_attributes(user_params) #ser user params based off of the form
+    render_wizard @user #the method will try to save it and if succeed will continue to the next step if not render a new form for the same step. 
+  end
+  
+  private
+    def user_params
+      params.permit(:email, :password, :name, :school, :twitter, :linkedin)
+    end
+  
+  def finish_wizard_path
+     root_path notice: 'Thanks you are all set'
   end
 end
